fd0006b16f79d3667963243be6c5ff3c574d9ea8
Tarun Dayma <tarundayma54@gmail.com>
2026-07-24 20:17:05 +0530

    Added Console UI

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import model.Expense;
 import service.ExpenseManager;
+import ui.ConsoleUI;
 
 import java.time.LocalDate;
 import java.util.Scanner;
@@ -11,16 +12,26 @@ public class Main {
 
         while (true) {
 
-            System.out.println("\n===== Smart Expense Tracker =====");
+//            ConsoleUI.title("SMART EXPENSE TRACKER");
+            ConsoleUI.dashboard(
+                    manager.getTotalExpenses(),
+                    manager.getTotalAmount()
+            );
+
             System.out.println("1. Add Expense");
-            System.out.println("2. View Expenses");
-            System.out.println("3. Exit");
-            System.out.print("Choose an option: ");
+            System.out.println("2. View All Expenses");
+            System.out.println("3. Delete Expense");
+            System.out.println("0. Exit");
+
+            ConsoleUI.line();
+            System.out.print("Enter your choice: ");
 
             int choice = sc.nextInt();
 
             switch (choice) {
                 case 1 -> {
+
+                    ConsoleUI.section("ADD NEW EXPENSE");
 
                     System.out.print("Enter Amount: ");
                     double amount = sc.nextDouble();
@@ -41,28 +52,45 @@ public class Main {
 
                     manager.addExpense(expense);
 
-                    System.out.println("\n✅ Expense Added Successfully!");
+                    ConsoleUI.success("Expense Added Successfully!");
                 }
                 case 2 -> {
 
-                    System.out.println("\n------ Expense List ------");
-
                     if (manager.getAllExpenses().isEmpty()) {
-                        System.out.println("No expenses found.");
+                        ConsoleUI.error("Expense not found.");
                     } else {
 
-                        for (Expense e : manager.getAllExpenses()) {
-                            System.out.println(e);
-                        }
+                        ConsoleUI.displayExpenses(manager.getAllExpenses());
                     }
                 }
                 case 3 -> {
+                    ConsoleUI.section("DELETE EXPENSE");
 
-                    System.out.println("Thank you for using Smart Expense Tracker!");
+                    if (manager.getAllExpenses().isEmpty()) {
+                        ConsoleUI.error("No expenses found");
+                        break;
+                    }
+
+                    ConsoleUI.displayExpenses(manager.getAllExpenses());
+
+                    System.out.println("Enter expense number");
+                    int number = sc.nextInt();
+
+                    boolean deleted = manager.deleteExpenseByIndex(number - 1);
+
+                    if(deleted) {
+                        ConsoleUI.success("Expense deleted successfully.");
+                    } else {
+                        ConsoleUI.error("Invalid expense number.");
+                    }
+                }
+                case 0 -> {
+
+                    System.out.println("Thank you for using Smart Expense Tracker 😉\nHave a good day!");
                     sc.close();
                     return;
                 }
-                default -> System.out.println("Invalid Choice!");
+                default -> ConsoleUI.error("Invalid choice.");
             }
         }
     }
